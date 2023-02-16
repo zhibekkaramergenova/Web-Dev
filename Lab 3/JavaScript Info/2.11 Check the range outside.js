9c50fdb4c6a98de2196a15ed95b0af2b37705c15
zhibekkaramergenova <80530342+zhibekkaramergenova@users.noreply.github.com>
2023-02-16 18:13:47 +0600

    Create 2.11 Check the range outside.js

---
+++ Lab 3/JavaScript Info/2.11 Check the range outside.js
@@ -0,0 +1,13 @@
+age = +prompt("Enter age","");
+if (!(age >= 14 && age <= 90)){
+    alert("good!");
+}
+else{
+    alert("not good!");
+}
+if (age < 14 || age > 90){
+    alert("good!");
+}
+else{
+    alert("not good!");
+}
