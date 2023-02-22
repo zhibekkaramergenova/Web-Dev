e03178f2c9ba18f9fae37c48fd543cd1f672dc8f
zhibekkaramergenova <80530342+zhibekkaramergenova@users.noreply.github.com>
2023-02-22 22:06:11 +0600

    Create product-list.component.ts

---
+++ Lab 4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { products } from '../products';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
